df57ed107dfcb47314b4a7a426db02927552edee
jlashmet <jlashmet@gmail.com>
2026-08-13 13:17:53 -1000

    Refine terrain camera pan

--- Assets/Scenes/Showcase/TerrainLookdev.CameraTune.cs
+++ Assets/Scenes/Showcase/TerrainLookdev.CameraTune.cs
@@ -9,7 +9,7 @@ namespace VoxelEngine.Showcase
             Camera camera = SceneCamera;
             camera.fieldOfView = 21.7f;
             camera.transform.position = new Vector3(-0.60f, 23.0f, -20.0f);
-            camera.transform.LookAt(new Vector3(-0.73f, 2.20f, 12.15f));
+            camera.transform.LookAt(new Vector3(-0.53f, 2.20f, 12.15f));
         }
     }
 }
