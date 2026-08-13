ff5cddffb3ad733ff1ea46c2fd6606861a96d666
jlashmet <jlashmet@gmail.com>
2026-08-13 13:12:06 -1000

    Tune terrain camera framing

---
+++ Assets/Scenes/Showcase/TerrainLookdev.CameraTune.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+namespace VoxelEngine.Showcase
+{
+    public sealed partial class TerrainLookdev
+    {
+        private void LateUpdate()
+        {
+            Camera camera = SceneCamera;
+            camera.fieldOfView = 21.7f;
+            camera.transform.position = new Vector3(-0.60f, 23.0f, -20.0f);
+            camera.transform.LookAt(new Vector3(-0.63f, 2.20f, 12.15f));
+        }
+    }
+}
