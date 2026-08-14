628368646f91a48276510bdf26971d20507b62f8
jlashmet <jlashmet@gmail.com>
2026-08-14 12:52:43 -1000

    refactor: import storage api in persistence tests

--- Assets/Tests/PlayMode/PersistenceTests.cs
+++ Assets/Tests/PlayMode/PersistenceTests.cs
@@ -3,6 +3,7 @@ using NUnit.Framework;
 using Unity.Collections;
 using Unity.Mathematics;
 using VoxelEngine.Core.Storage;
+using VoxelEngine.Storage.Api;
 using VoxelEngine.Net.Server;
 
 namespace VoxelEngine.Tests.PlayMode
