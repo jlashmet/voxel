2faf90096996ae22a94a1bca2049f0bf66d45f9e
jlashmet <jlashmet@gmail.com>
2026-08-15 04:30:19 -1000

    refactor(net): remove Storage runtime mip ownership from event application

--- Assets/VoxelEngine/Net/Client/EventApplication.cs
+++ Assets/VoxelEngine/Net/Client/EventApplication.cs
@@ -2,8 +2,6 @@ using System.Runtime.CompilerServices;
 using Unity.Collections;
 using Unity.Mathematics;
 using VoxelEngine.Edits.Api;
-using VoxelEngine.Core.Occupancy;
-using VoxelEngine.Core.Storage;
 using VoxelEngine.Storage.Api;
 
 namespace VoxelEngine.Net.Client
@@ -17,37 +15,6 @@ namespace VoxelEngine.Net.Client
             out NativeList<int3> affectedBricks)
         {
             return applier.TryApply(storage, in evt, out affectedBricks);
-        }
-
-        /// <summary>
-        /// After applying events, trigger mip rebuild and irradiance probe invalidation
-        /// for all affected regions. Called after a batch of events is applied each tick.
-        /// </summary>
-        public static void TriggerInfrastructureUpdates(
-            ref RegionTable table,
-            in BrickPool pool,
-            in NativeArray<int3> affectedRegions,
-            int mipLevelCount)
-        {
-            // Batched mip rebuild over dirty regions (T026).
-            //
-            // Each region owns its own flattened pyramid, so an edit refreshes storage that
-            // travels with the region and survives until the region itself is evicted. A
-            // region that has never had mips allocated gets them here on first touch.
-            for (int i = 0; i < affectedRegions.Length; i++)
-            {
-                var regionCoord = affectedRegions[i];
-                if (!table.TryGetRegion(regionCoord, out var region))
-                    continue;
-
-                if (!region.HasMips)
-                {
-                    region.AllocateMips(mipLevelCount, Allocator.Persistent);
-                    table.CommitRegion(in region);
-                }
-
-                MipBuilder.RebuildRegion(in pool, ref region);
-            }
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
