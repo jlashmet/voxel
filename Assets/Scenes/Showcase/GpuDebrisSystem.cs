ec5aa7c2950528afb90faab0437cc263cd355759
architecture-refactor-ci <architecture-refactor-ci@users.noreply.github.com>
2026-08-15 17:04:29 +0000

    refactor(showcase): remove stale GpuDebris Storage Runtime import

--- Assets/Scenes/Showcase/GpuDebrisSystem.cs
+++ Assets/Scenes/Showcase/GpuDebrisSystem.cs
@@ -3,7 +3,6 @@ using System.Runtime.InteropServices;
 using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Rendering;
-using VoxelEngine.Storage.Runtime;
 using VoxelEngine.Storage.Api;
 using VoxelEngine.Rendering.Runtime;
 
