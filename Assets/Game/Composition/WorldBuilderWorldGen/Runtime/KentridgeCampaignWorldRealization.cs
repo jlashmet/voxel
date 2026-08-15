3ecacda380830b9a89c764382a467ee8bc2bd0a8
jlashmet <jlashmet@gmail.com>
2026-08-15 09:39:55 -1000

    chore: import world realization composition API explicitly

--- Assets/Game/Composition/WorldBuilderWorldGen/Runtime/KentridgeCampaignWorldRealization.cs
+++ Assets/Game/Composition/WorldBuilderWorldGen/Runtime/KentridgeCampaignWorldRealization.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Game.Composition.WorldBuilderWorldGen;
 using Game.WorldBuilder.Api;
 using Game.WorldBuilder.Runtime;
 using MountingForce.WorldGen;
