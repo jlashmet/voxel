fd123a5d36220906b9647c416807bd2edacc9c11
jlashmet <jlashmet@gmail.com>
2026-08-15 05:36:25 -1000

    refactor(storage): expose material presentation snapshot

--- Assets/VoxelEngine/Core/Storage/MaterialPalette.cs
+++ Assets/VoxelEngine/Core/Storage/MaterialPalette.cs
@@ -16,7 +16,7 @@ namespace VoxelEngine.Core.Storage
     ///   - The type of debris generated on destruction (none, particles, physics bodies)
     ///   - Whether the material spreads fire/chemical reactions to adjacent bricks
     /// </summary>
-    public unsafe struct MaterialPalette : IMaterialAuthoringCatalogue
+    public unsafe struct MaterialPalette : IMaterialAuthoringCatalogue, IMaterialPresentationCatalogue
     {
         /// <summary>Number of registered materials in the palette.</summary>
         public int Count => _count;
@@ -67,6 +67,11 @@ namespace VoxelEngine.Core.Storage
         public bool AllowsCoating(byte materialIndex, byte coatingId) =>
             IsRegistered(materialIndex) && coatingId < 32
             && (_allowedCoatings[materialIndex] & (1u << coatingId)) != 0;
+
+        public MaterialPaletteView PresentationView => MaterialPaletteView.Capture(in this);
+
+        public static implicit operator MaterialPaletteView(MaterialPalette source) =>
+            MaterialPaletteView.Capture(in source);
 
         /// <summary>Look up the destruction class for a given material index.</summary>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
