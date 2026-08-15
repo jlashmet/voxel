ea9b34022a70b6570470888557c3a2d649b8eb03
jlashmet <jlashmet@gmail.com>
2026-08-15 11:37:57 -1000

    test: cover compiled hierarchy generation plans

--- Assets/Tests/EditMode/WorldHierarchyBlueprintTests.cs
+++ Assets/Tests/EditMode/WorldHierarchyBlueprintTests.cs
@@ -31,7 +31,7 @@ namespace VoxelEngine.Tests.EditMode
                 .RequireCapability(SiteCapability.Interior)
                 .RequireCapability(SiteCapability.PublicExit));
 
-            game.World.RequireSite("first-destination", region, site => site
+            SiteRef destination = game.World.RequireSite("first-destination", region, site => site
                 .Archetype(SiteArchetype.Ruin)
                 .DifferentSiteFrom(pub)
                 .ReachableFrom(pub, TraversalProfile.NormalParty));
@@ -58,6 +58,39 @@ namespace VoxelEngine.Tests.EditMode
             Assert.That(townNode.Dependencies, Does.Contain("route:main-route"));
             Assert.That(pubNode.Dependencies, Is.EquivalentTo(new[] { "settlement:starting-town" }));
             Assert.That(destinationNode.Dependencies, Is.EquivalentTo(new[] { "region:starting-region" }));
+
+            WorldRegionPlan regionPlan = graph.HierarchyPlan.Regions.Single(value => value.Region.Equals(region));
+            Assert.That(regionPlan.Biome, Is.EqualTo(BiomeFamily.TemperateForest));
+            Assert.That(regionPlan.Routes, Is.EquivalentTo(new[] { route }));
+            Assert.That(regionPlan.Settlements, Is.EquivalentTo(new[] { town }));
+            Assert.That(regionPlan.RegionOwnedSites, Is.EquivalentTo(new[] { destination }));
+
+            WorldRoutePlan routePlan = graph.HierarchyPlan.Routes.Single(value => value.Route.Equals(route));
+            Assert.That(routePlan.Region, Is.EqualTo(region));
+            Assert.That(routePlan.Kind, Is.EqualTo(RouteKind.TradeRoad));
+            Assert.That(routePlan.Importance, Is.EqualTo(RouteImportance.Primary));
+            WorldRouteAccessPlan routeAccess = routePlan.SettlementAccess.Single();
+            Assert.That(routeAccess.Settlement, Is.EqualTo(town));
+            Assert.That(routeAccess.ConnectorLengthMetres.Minimum, Is.EqualTo(0));
+            Assert.That(routeAccess.ConnectorLengthMetres.Maximum, Is.EqualTo(80));
+
+            WorldSettlementPlan townPlan = graph.HierarchyPlan.Settlements.Single(value => value.Settlement.Equals(town));
+            Assert.That(townPlan.Region, Is.EqualTo(region));
+            Assert.That(townPlan.Archetype, Is.EqualTo(SettlementArchetype.Town));
+            Assert.That(townPlan.HasPopulationRange, Is.True);
+            Assert.That(townPlan.Population.Minimum, Is.EqualTo(150));
+            Assert.That(townPlan.Population.Maximum, Is.EqualTo(450));
+            Assert.That(townPlan.Sites, Is.EquivalentTo(new[] { pub }));
+            Assert.That(townPlan.RouteAccess.Single(), Is.SameAs(routeAccess),
+                "Route and settlement plans must share the same compiled access requirement object.");
+
+            WorldSitePlacementPlan pubPlacement = graph.HierarchyPlan.SitePlacements.Single(value => value.Site.Equals(pub));
+            Assert.That(pubPlacement.Kind, Is.EqualTo(SitePlacementKind.Settlement));
+            Assert.That(pubPlacement.Settlement, Is.EqualTo(town));
+
+            WorldSitePlacementPlan destinationPlacement = graph.HierarchyPlan.SitePlacements.Single(value => value.Site.Equals(destination));
+            Assert.That(destinationPlacement.Kind, Is.EqualTo(SitePlacementKind.Region));
+            Assert.That(destinationPlacement.Region, Is.EqualTo(region));
         }
 
         [Test]
