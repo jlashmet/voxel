92503b83a17a51058c5a87883d4abf089320d125
jlashmet <jlashmet@gmail.com>
2026-08-16 00:23:16 -1000

    test(structures): cover runtime keep readiness

---
+++ Assets/Tests/EditMode/CastleRuntimeReadinessTests.cs
@@ -0,0 +1,72 @@
+using NUnit.Framework;
+using Unity.Mathematics;
+using VoxelEngine.Structures.Api;
+
+namespace VoxelEngine.Tests.EditMode
+{
+    public sealed class CastleRuntimeReadinessTests
+    {
+        [Test]
+        public void RuntimeReadinessRejectsMissingKeepCirculationBeforeVoxelMutation()
+        {
+            CastlePlan plan = CastlePlanner.Create(int3.zero, 301u);
+            CastleTopologyPlan topology = CastleLayoutPlanner.Create(plan.Seed);
+            topology.KeepPlacement = CastleKeepPlacement.Central;
+            CastleSpatialPlan spatial = CastleSpatialPlanner.Create(in plan, in topology);
+
+            spatial = CastleSpatialPlanCompletion.AttachTowerVariation(in plan, spatial);
+            spatial = CastleSpatialPlanCompletion.AttachKeepFloors(in plan, spatial);
+            // Deliberately skip AttachKeepCirculation.
+            spatial = CastleSpatialPlanCompletion.AttachCourtyardBuildings(in plan, spatial);
+            spatial = CastleSpatialPlanCompletion.AttachDungeon(in plan, spatial);
+            spatial = CastleSpatialPlanCompletion.AttachCave(in plan, spatial);
+            spatial = CastleSpatialPlanCompletion.AttachCaveDecoration(in plan, spatial);
+            spatial = CastleSpatialPlanCompletion.AttachLandscape(in plan, spatial);
+
+            CastleBuildPreflightResult readiness = CastleBuildPreflight.EvaluateRuntimeReady(
+                in plan, spatial, long.MaxValue);
+
+            Assert.AreEqual(CastleBuildPreflightIssue.IncompleteSpatialPlan, readiness.Issue);
+            Assert.AreEqual(
+                CastleSpatialBuildReadinessIssue.InvalidKeepCirculationPlan,
+                readiness.ReadinessIssue);
+        }
+
+        [Test]
+        public void RuntimeReadinessRejectsMissingKeepAnnexPlanBeforeVoxelMutation()
+        {
+            CastlePlan plan = CastlePlanner.Create(int3.zero, 307u);
+            CastleTopologyPlan topology = CastleLayoutPlanner.Create(plan.Seed);
+            topology.KeepPlacement = CastleKeepPlacement.Central;
+            topology.HasKeepAnnexPlan = false;
+            topology.KeepAnnexes = default;
+
+            CastleSpatialPlan spatial = CastleSpatialPlanner.Create(in plan, in topology);
+            spatial = CastleSpatialPlanCompletion.CompleteResolved(in plan, spatial);
+
+            CastleBuildPreflightResult readiness = CastleBuildPreflight.EvaluateRuntimeReady(
+                in plan, spatial, long.MaxValue);
+
+            Assert.AreEqual(CastleBuildPreflightIssue.IncompleteSpatialPlan, readiness.Issue);
+            Assert.AreEqual(
+                CastleSpatialBuildReadinessIssue.MissingKeepAnnexPlan,
+                readiness.ReadinessIssue);
+        }
+
+        [Test]
+        public void RuntimeReadinessAcceptsCompletedKeepMetadata()
+        {
+            CastlePlan plan = CastlePlanner.Create(int3.zero, 313u);
+            CastleTopologyPlan topology = CastleLayoutPlanner.Create(plan.Seed);
+            topology.KeepPlacement = CastleKeepPlacement.Central;
+            CastleSpatialPlan spatial = CastleSpatialPlanner.Create(in plan, in topology);
+            spatial = CastleSpatialPlanCompletion.CompleteResolved(in plan, spatial);
+
+            CastleBuildPreflightResult readiness = CastleBuildPreflight.EvaluateRuntimeReady(
+                in plan, spatial, long.MaxValue);
+
+            Assert.IsTrue(readiness.IsValid, readiness.ReadinessIssue.ToString());
+            Assert.AreEqual(CastleSpatialBuildReadinessIssue.None, readiness.ReadinessIssue);
+        }
+    }
+}
