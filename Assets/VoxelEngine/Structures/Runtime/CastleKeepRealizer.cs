b2d6b668bf4552981f7321607ca66ce3db01257f
jlashmet <jlashmet@gmail.com>
2026-08-16 01:18:44 -1000

    refactor(structures): delegate keep fenestration

--- Assets/VoxelEngine/Structures/Runtime/CastleKeepRealizer.cs
+++ Assets/VoxelEngine/Structures/Runtime/CastleKeepRealizer.cs
@@ -66,7 +66,8 @@ namespace VoxelEngine.Structures.Runtime
                     break;
 
                 case 4:
-                    BuildWindows(ref brush, in plan, min, size, baseY, floors);
+                    CastleKeepFenestrationRealizer.Build(
+                        ref brush, in plan, min, size, baseY, floors);
                     break;
 
                 case 5:
@@ -188,32 +189,6 @@ namespace VoxelEngine.Structures.Runtime
             const int stairRadius = 22;
             brush.SpiralStair(stairX, baseY + 2, stairZ, stairRadius,
                               floors * plan.FloorHeight, Mat.Stone);
-        }
-
-        private static void BuildWindows(ref VoxelBrush brush, in CastlePlan plan,
-                                         int3 min, int3 size, int baseY, int floors)
-        {
-            for (int f = 0; f < floors; f++)
-            {
-                int y = baseY + f * plan.FloorHeight + 12;
-                int height = f == 1 ? plan.FloorHeight - 14 : plan.FloorHeight - 18;
-                for (int i = 0; i < 3; i++)
-                {
-                    int x = min.x + size.x / 4 + i * size.x / 4 - 8;
-                    bool mainEntrance = f == 0 && i == 1;
-                    if (!mainEntrance)
-                    {
-                        brush.Arch(new int3(x, y, min.z), 16, height, 9, 2, Mat.Empty);
-                        brush.Box(new int3(x + 3, y + 4, min.z + 2),
-                                  new int3(10, height - 10, 2), Mat.LitWindow);
-                        brush.Box(new int3(x + 7, y + 5, min.z + 1),
-                                  new int3(2, height - 12, 3), Mat.DarkStone);
-                        brush.Box(new int3(x + 3, y + height / 2, min.z + 1),
-                                  new int3(10, 2, 3), Mat.DarkStone);
-                    }
-                    brush.Arch(new int3(x, y, min.z + size.z - 8), 16, height, 9, 2, Mat.Empty);
-                }
-            }
         }
 
         private static void BuildFacade(ref VoxelBrush brush, in CastlePlan plan,
