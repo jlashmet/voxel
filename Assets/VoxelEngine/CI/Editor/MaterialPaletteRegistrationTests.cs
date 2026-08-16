7638b60b951ee7a38251dbea4cfe6bcb5657b05b
jlashmet <jlashmet@gmail.com>
2026-08-15 20:29:25 -1000

    Test generic material placement properties

--- Assets/VoxelEngine/CI/Editor/MaterialPaletteRegistrationTests.cs
+++ Assets/VoxelEngine/CI/Editor/MaterialPaletteRegistrationTests.cs
@@ -22,5 +22,27 @@ namespace VoxelEngine.CI
             Assert.That(palette.IsDestructible(16), Is.False);
             Assert.That(palette.IsFlammable(16), Is.False);
         }
+
+        [Test]
+        public void Registration_RoundTripsSemanticFreePlacementProperties()
+        {
+            MaterialPalette palette = default;
+            var definition = new MaterialDefinition(
+                materialId: 7,
+                hardness: 80,
+                destructionClass: DestructionClass.Crumble,
+                defaultSurfaceStyle: SurfaceStyles.Smooth,
+                allowedCoatings: 1u << Coatings.Soot,
+                flammable: false,
+                placementSurfaceStyle: SurfaceStyles.Rounded,
+                placementCoating: Coatings.Soot);
+
+            palette.Register(in definition);
+
+            Assert.That(palette.GetPlacementSurfaceStyle(7), Is.EqualTo(SurfaceStyles.Rounded));
+            Assert.That(palette.GetPlacementCoating(7), Is.EqualTo(Coatings.Soot));
+            Assert.That(palette.GetPlacementSurfaceStyle(6), Is.EqualTo(SurfaceStyles.MaterialDefault));
+            Assert.That(palette.GetPlacementCoating(6), Is.EqualTo(Coatings.None));
+        }
     }
 }
