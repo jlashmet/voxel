5af22447243d9b049a621967fed1982c7e5d707c
jlashmet <jlashmet@gmail.com>
2026-08-16 00:36:42 -1000

    test(structures): cover castle site plan validation

---
+++ Assets/Tests/EditMode/CastleSitePlanValidationTests.cs
@@ -0,0 +1,82 @@
+using NUnit.Framework;
+using VoxelEngine.Structures.Api;
+
+namespace VoxelEngine.Tests.EditMode
+{
+    public sealed class CastleSitePlanValidationTests
+    {
+        [Test]
+        public void GeneratedSitePlansAreStructurallyValid()
+        {
+            for (uint seed = 1; seed <= 256; seed++)
+            {
+                CastleSitePlan site = CastleSitePlanner.Create(seed);
+                Assert.IsTrue(
+                    CastleSitePlanValidator.TryValidate(in site, out CastleSitePlanIssue issue),
+                    $"seed {seed}: {issue}");
+            }
+        }
+
+        [Test]
+        public void TopologyRejectsMissingDefaultSitePlan()
+        {
+            CastleTopologyPlan topology = CastleLayoutPlanner.Create(97u);
+            topology.Site = default;
+
+            Assert.IsFalse(
+                CastleTopologyPlanValidator.TryValidate(
+                    in topology, out CastleTopologyPlanIssue issue));
+            Assert.AreEqual(CastleTopologyPlanIssue.InvalidSitePlan, issue);
+        }
+
+        [Test]
+        public void SitePlanRejectsActiveCourtyardPatternWithoutSeed()
+        {
+            CastleSitePlan generated = CastleSitePlanner.Create(101u);
+            CastleSiteGeometryPlan geometry = generated.Geometry;
+            var invalid = new CastleSitePlan(
+                generated.GrassPatternSeed,
+                generated.GrassCoveragePercent,
+                0u,
+                generated.CourtyardStonePercent,
+                in geometry);
+
+            Assert.IsFalse(
+                CastleSitePlanValidator.TryValidate(
+                    in invalid, out CastleSitePlanIssue issue));
+            Assert.AreEqual(CastleSitePlanIssue.MissingCourtyardPatternSeed, issue);
+        }
+
+        [Test]
+        public void SitePlanRejectsDefaultGeometryEvenWithValidSeeds()
+        {
+            CastleSitePlan generated = CastleSitePlanner.Create(103u);
+            CastleSiteGeometryPlan geometry = default;
+            var invalid = new CastleSitePlan(
+                generated.GrassPatternSeed,
+                generated.GrassCoveragePercent,
+                generated.CourtyardPatternSeed,
+                generated.CourtyardStonePercent,
+                in geometry);
+
+            Assert.IsFalse(
+                CastleSitePlanValidator.TryValidate(
+                    in invalid, out CastleSitePlanIssue issue));
+            Assert.AreEqual(CastleSitePlanIssue.InvalidGeometry, issue);
+        }
+
+        [Test]
+        public void DisabledCourtyardPatternDoesNotRequireSeed()
+        {
+            CastleSitePlan generated = CastleSitePlanner.Create(107u);
+            var site = new CastleSitePlan(
+                generated.GrassPatternSeed,
+                generated.GrassCoveragePercent);
+
+            Assert.IsTrue(
+                CastleSitePlanValidator.TryValidate(
+                    in site, out CastleSitePlanIssue issue),
+                issue.ToString());
+        }
+    }
+}
