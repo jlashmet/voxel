61d5db217b5d2482dd421a0db7822decde9381af
jlashmet <jlashmet@gmail.com>
2026-08-15 22:13:24 -1000

    test(async-geometry): friend PlayMode geometry fixtures

--- Assets/VoxelEngine/Rendering/Runtime/AssemblyInfo.cs
+++ Assets/VoxelEngine/Rendering/Runtime/AssemblyInfo.cs
@@ -1,4 +1,5 @@
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("VoxelEngine.Tests.EditMode")]
+[assembly: InternalsVisibleTo("VoxelEngine.Tests.PlayMode")]
 [assembly: InternalsVisibleTo("VoxelEngine.CI.PlayMode")]
