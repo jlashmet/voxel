dd2122b07a5e4543a0183c230025bff88b6a272d
jlashmet <jlashmet@gmail.com>
2026-08-16 00:07:22 -1000

    feat(structures): carry planned keep floors

--- Assets/VoxelEngine/Structures/Api/CastleSpatialPlan.cs
+++ Assets/VoxelEngine/Structures/Api/CastleSpatialPlan.cs
@@ -46,6 +46,7 @@ namespace VoxelEngine.Structures.Api
         public bool HasWell { get; }
         public int2 WellCentre { get; }
         public CastleCourtyardBuildingSpec[] CourtyardBuildings { get; }
+        public CastleKeepFloorPlan[] KeepFloors { get; }
         public DungeonPlan Dungeon { get; }
         public CavePlan Cave { get; }
         public int2 KeepCentre { get; }
@@ -202,6 +203,45 @@ namespace VoxelEngine.Structures.Api
             CavePlan cave,
             int2 keepCentre,
             bool keepRequiresTerrainResolution)
+            : this(
+                in topology,
+                outerWardVertices,
+                innerWardVertices,
+                towers,
+                in primaryGate,
+                hasPosternGate,
+                in posternGate,
+                hasInnerGate,
+                in innerGate,
+                hasWell,
+                wellCentre,
+                courtyardBuildings,
+                Array.Empty<CastleKeepFloorPlan>(),
+                dungeon,
+                cave,
+                keepCentre,
+                keepRequiresTerrainResolution)
+        {
+        }
+
+        internal CastleSpatialPlan(
+            in CastleTopologyPlan topology,
+            int2[] outerWardVertices,
+            int2[] innerWardVertices,
+            CastleTowerPlacementSpec[] towers,
+            in CastleGatePlacementSpec primaryGate,
+            bool hasPosternGate,
+            in CastleGatePlacementSpec posternGate,
+            bool hasInnerGate,
+            in CastleGatePlacementSpec innerGate,
+            bool hasWell,
+            int2 wellCentre,
+            CastleCourtyardBuildingSpec[] courtyardBuildings,
+            CastleKeepFloorPlan[] keepFloors,
+            DungeonPlan dungeon,
+            CavePlan cave,
+            int2 keepCentre,
+            bool keepRequiresTerrainResolution)
         {
             Topology = topology;
             OuterWardVertices = outerWardVertices;
@@ -218,6 +258,7 @@ namespace VoxelEngine.Structures.Api
             HasWell = hasWell;
             WellCentre = wellCentre;
             CourtyardBuildings = courtyardBuildings ?? Array.Empty<CastleCourtyardBuildingSpec>();
+            KeepFloors = keepFloors ?? Array.Empty<CastleKeepFloorPlan>();
             Dungeon = dungeon;
             Cave = cave;
             KeepCentre = keepCentre;
