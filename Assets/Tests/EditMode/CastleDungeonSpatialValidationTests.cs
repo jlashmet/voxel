6df4720f6cdbeb990d6006fdb1f9cca98ede6864
jlashmet <jlashmet@gmail.com>
2026-08-16 00:08:43 -1000

    test(structures): lock dungeon castle attachment validation

--- Assets/Tests/EditMode/CastleDungeonSpatialValidationTests.cs
+++ Assets/Tests/EditMode/CastleDungeonSpatialValidationTests.cs
@@ -62,5 +62,41 @@ namespace VoxelEngine.Tests.EditMode
             Assert.AreEqual(CastleSpatialPlanIssue.None, readiness.SpatialPlanIssue);
             Assert.IsFalse(readiness.IsValid);
         }
+
+        [Test]
+        public void DungeonFromDifferentCastleOriginIsRejectedAsAttachmentMismatch()
+        {
+            CastlePlan dimensions = CastlePlanner.Create(new int3(200, 64, 300), 149u);
+            CastleTopologyPlan topology = CastleLayoutPlanner.Create(149u);
+            topology.Perimeter = CastlePerimeterKind.Rectangular;
+            topology.Wards = CastleWardPattern.SingleWard;
+            topology.KeepPlacement = CastleKeepPlacement.Central;
+            topology.DesiredTowerCount = 4;
+            topology.HasPosternGate = false;
+
+            CastleSpatialPlan spatial = CastleSpatialPlanner.Create(in dimensions, in topology);
+            CastleSpatialPlan completed = CastleSpatialPlanCompletion.CompleteResolved(
+                in dimensions, spatial);
+            Assert.IsTrue(
+                DungeonPlanValidator.TryValidate(completed.Dungeon, out DungeonPlanIssue dungeonIssue),
+                dungeonIssue.ToString());
+
+            CastlePlan shiftedCastle = dimensions;
+            shiftedCastle.Centre += new int3(32, 0, -16);
+
+            Assert.IsFalse(
+                CastleSpatialPlanValidator.TryValidate(
+                    in shiftedCastle, completed, out CastleSpatialPlanIssue spatialIssue));
+            Assert.AreEqual(CastleSpatialPlanIssue.DungeonEntranceMismatch, spatialIssue);
+
+            CastleBuildPreflightResult readiness = CastleBuildPreflight.EvaluateRuntimeReady(
+                in shiftedCastle, completed, long.MaxValue);
+            Assert.AreEqual(CastleBuildPreflightIssue.IncompleteSpatialPlan, readiness.Issue);
+            Assert.AreEqual(
+                CastleSpatialBuildReadinessIssue.DungeonEntranceMismatch,
+                readiness.ReadinessIssue);
+            Assert.AreEqual(CastleSpatialPlanIssue.None, readiness.SpatialPlanIssue);
+            Assert.IsFalse(readiness.IsValid);
+        }
     }
 }
