0d27404b4174ee5d6f42695c1832ca25c526772f
jlashmet <jlashmet@gmail.com>
2026-08-15 22:25:35 -1000

    test: require configured pipeline material shader

--- Assets/Tests/EditMode/PlaceholderHumanoidMaterialTests.cs
+++ Assets/Tests/EditMode/PlaceholderHumanoidMaterialTests.cs
@@ -2,6 +2,7 @@ using System.Linq;
 using NUnit.Framework;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 namespace VoxelEngine.Tests.EditMode
 {
@@ -9,8 +10,12 @@ namespace VoxelEngine.Tests.EditMode
     {
         [TestCase("Assets/ThirdParty/PlaceholderHumanoids/Models/Male_Adult_01.fbx")]
         [TestCase("Assets/ThirdParty/PlaceholderHumanoids/Models/Female_Adult_01.fbx")]
-        public void PlaceholderBody_ActiveMeshesHaveAssignedMaterials(string path)
+        public void PlaceholderBody_ActiveMeshesUseConfiguredPipelineMaterial(string path)
         {
+            AssetDatabase.ImportAsset(
+                path,
+                ImportAssetOptions.ForceUpdate | ImportAssetOptions.ForceSynchronousImport);
+
             var model = AssetDatabase.LoadAssetAtPath<GameObject>(path);
             Assert.That(model, Is.Not.Null, $"Unity could not load placeholder body {path}");
 
@@ -20,6 +25,9 @@ namespace VoxelEngine.Tests.EditMode
             Assert.That(activeRenderers.Length, Is.GreaterThan(0),
                 $"{path} has no active skinned mesh renderer");
 
+            var pipeline = GraphicsSettings.currentRenderPipeline ?? GraphicsSettings.defaultRenderPipeline;
+            var expectedMaterial = pipeline?.defaultMaterial;
+
             foreach (var renderer in activeRenderers)
             {
                 Assert.That(renderer.sharedMaterials.Length, Is.GreaterThan(0),
@@ -28,6 +36,12 @@ namespace VoxelEngine.Tests.EditMode
                     $"{path} renderer {renderer.name} contains an unassigned material slot");
                 Assert.That(renderer.sharedMaterials.All(material => material.shader != null), Is.True,
                     $"{path} renderer {renderer.name} contains a material with no shader");
+
+                if (expectedMaterial != null)
+                {
+                    Assert.That(renderer.sharedMaterials.All(material => material.shader == expectedMaterial.shader), Is.True,
+                        $"{path} renderer {renderer.name} is not using the configured render pipeline's default shader");
+                }
             }
         }
 
