8f5449c1bf157edb52ec89debdf97e23e7c30d1a
jlashmet <jlashmet@gmail.com>
2026-08-16 00:29:25 -1000

    test(structures): cover keep facade planning frame

---
+++ Assets/Tests/EditMode/CastleKeepFacadeTests.cs
@@ -0,0 +1,97 @@
+using NUnit.Framework;
+using Unity.Mathematics;
+using VoxelEngine.Structures.Api;
+
+namespace VoxelEngine.Tests.EditMode
+{
+    public sealed class CastleKeepFacadeTests
+    {
+        [Test]
+        public void FacadePlannerFacesThePrimaryGateFromKeepCentre()
+        {
+            AssertFace(new int2(0, -120), CastleKeepFace.South);
+            AssertFace(new int2(140, -20), CastleKeepFace.East);
+            AssertFace(new int2(15, 130), CastleKeepFace.North);
+            AssertFace(new int2(-150, 10), CastleKeepFace.West);
+        }
+
+        [Test]
+        public void ExactDiagonalTiePrefersZFacadeForStableLegacyBias()
+        {
+            var southEast = new CastleGatePlacementSpec
+            {
+                Centre = new int2(90, -90),
+                Outward = new float2(1f, -1f),
+            };
+            var northWest = new CastleGatePlacementSpec
+            {
+                Centre = new int2(-90, 90),
+                Outward = new float2(-1f, 1f),
+            };
+
+            Assert.AreEqual(
+                CastleKeepFace.South,
+                CastleKeepFacadePlanner.FacingPrimaryGate(int2.zero, in southEast));
+            Assert.AreEqual(
+                CastleKeepFace.North,
+                CastleKeepFacadePlanner.FacingPrimaryGate(int2.zero, in northWest));
+        }
+
+        [Test]
+        public void FacadeFrameUsesTheCorrectRectangularKeepExtent()
+        {
+            var plan = new CastlePlan
+            {
+                KeepHalfX = 100,
+                KeepHalfZ = 70,
+            };
+
+            Assert.AreEqual(
+                new int2(12, -70),
+                CastleKeepFacadeFrame.For(CastleKeepFace.South)
+                    .PointFromFacade(in plan, 12, 0));
+            Assert.AreEqual(
+                new int2(100, 12),
+                CastleKeepFacadeFrame.For(CastleKeepFace.East)
+                    .PointFromFacade(in plan, 12, 0));
+            Assert.AreEqual(
+                new int2(-12, 70),
+                CastleKeepFacadeFrame.For(CastleKeepFace.North)
+                    .PointFromFacade(in plan, 12, 0));
+            Assert.AreEqual(
+                new int2(-100, -12),
+                CastleKeepFacadeFrame.For(CastleKeepFace.West)
+                    .PointFromFacade(in plan, 12, 0));
+        }
+
+        [Test]
+        public void PositiveInsetAlwaysMovesTowardKeepInterior()
+        {
+            var plan = new CastlePlan
+            {
+                KeepHalfX = 100,
+                KeepHalfZ = 70,
+            };
+
+            CastleKeepFacadeFrame east = CastleKeepFacadeFrame.For(CastleKeepFace.East);
+            CastleKeepFacadeFrame north = CastleKeepFacadeFrame.For(CastleKeepFace.North);
+
+            Assert.AreEqual(new int2(80, 0), east.PointFromFacade(in plan, 0, 20));
+            Assert.AreEqual(new int2(0, 50), north.PointFromFacade(in plan, 0, 20));
+            Assert.AreEqual(new int2(-1, 0), east.Inward);
+            Assert.AreEqual(new int2(0, -1), north.Inward);
+        }
+
+        private static void AssertFace(int2 gateCentre, CastleKeepFace expected)
+        {
+            var gate = new CastleGatePlacementSpec
+            {
+                Centre = gateCentre,
+                Outward = math.normalizesafe(new float2(gateCentre.x, gateCentre.y)),
+            };
+            Assert.AreEqual(
+                expected,
+                CastleKeepFacadePlanner.FacingPrimaryGate(int2.zero, in gate));
+        }
+    }
+}
