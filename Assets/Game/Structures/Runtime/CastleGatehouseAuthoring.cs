5444dd3c4907e46780ed4f8337be7765e9a1f04f
jlashmet <jlashmet@gmail.com>
2026-08-16 22:28:44 -1000

    fix: use canonical castle component config for gatehouse

--- Assets/Game/Structures/Runtime/CastleGatehouseAuthoring.cs
+++ Assets/Game/Structures/Runtime/CastleGatehouseAuthoring.cs
@@ -10,7 +10,7 @@ namespace Game.Structures.Runtime
     {
         public static void Author(IStructureAuthoringSession authoring, in CastlePlan plan)
         {
-            CastleConfig config = CastlePresets.Compatibility(in plan);
+            CastleComponentConfig config = CastleCompatibilityComponents.Resolve(in plan);
             Author(
                 authoring,
                 in plan,
