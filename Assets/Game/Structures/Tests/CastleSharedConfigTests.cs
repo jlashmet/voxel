a4780d3a6a751539ee6761cc2e1c1edf99a110ef
jlashmet <jlashmet@gmail.com>
2026-08-16 22:30:51 -1000

    test: validate canonical castle compatibility config

--- Assets/Game/Structures/Tests/CastleSharedConfigTests.cs
+++ Assets/Game/Structures/Tests/CastleSharedConfigTests.cs
@@ -1,3 +1,4 @@
+using Game.Materials.Api;
 using Game.Structures.Api;
 using Game.Structures.Runtime;
 using NUnit.Framework;
@@ -12,17 +13,22 @@ namespace Game.Structures.Tests
         public void CompatibilityProjectionPreservesLegacyPlanDimensions()
         {
             CastlePlan plan = CastlePlanner.Plan(new int3(120, 40, -80), 0x12345678u);
-            CastleConfig config = CastlePresets.Compatibility(in plan);
-            CastlePlan resolved = config.ResolvePlan();
+            CastleComponentConfig config = CastleCompatibilityComponents.Resolve(in plan);
 
             Assert.Multiple(() =>
             {
                 Assert.IsTrue(config.IsWellFormed);
 
+                Assert.AreEqual(plan.BaileyHalfX * 2, config.BaileyFootprint.Primary.Size.x);
+                Assert.AreEqual(plan.BaileyHalfZ * 2, config.BaileyFootprint.Primary.Size.y);
                 Assert.AreEqual(plan.KeepHalfX * 2 + 12, config.KeepFoundation.Primary.Size.x);
                 Assert.AreEqual(plan.KeepHalfZ * 2 + 12, config.KeepFoundation.Primary.Size.y);
                 Assert.AreEqual(30, config.KeepFoundation.FoundationDepth);
                 Assert.AreEqual(4, config.KeepFoundationTopOffset);
+                Assert.AreEqual(plan.KeepHeight, config.KeepWalls.Height);
+                Assert.AreEqual(8, config.KeepWalls.Thickness);
+                Assert.AreEqual(plan.Floors, config.KeepFloors.FloorCount);
+                Assert.AreEqual(plan.FloorHeight, config.KeepFloors.LevelHeight);
 
                 Assert.AreEqual(plan.BaileyHalfX * 2, config.CurtainWallX.Length);
                 Assert.AreEqual(plan.BaileyHalfZ * 2, config.CurtainWallZ.Length);
@@ -40,18 +46,23 @@ namespace Game.Structures.Tests
                 Assert.AreEqual(plan.GateTowerRadius, config.GateTowers.Radius);
                 Assert.AreEqual(plan.GateTowerHeight, config.GateTowers.Height);
 
+                Assert.AreEqual(StructureOpeningKind.Arch, config.MainGate.Kind);
                 Assert.AreEqual(CastleLayout.FrontGateWidth, config.MainGate.Width);
                 Assert.AreEqual(CastleLayout.FrontGateHeight, config.MainGate.Height);
                 Assert.AreEqual(26, config.CurtainBattlements.MerlonWidth);
                 Assert.AreEqual(18, config.CurtainBattlements.GapWidth);
                 Assert.AreEqual(20, config.CurtainBattlements.MerlonHeight);
+                Assert.AreEqual(18, config.GatehouseBattlements.MerlonWidth);
+                Assert.AreEqual(12, config.GatehouseBattlements.MerlonHeight);
 
-                Assert.AreEqual(plan.BaileyHalfX, resolved.BaileyHalfX);
-                Assert.AreEqual(plan.BaileyHalfZ, resolved.BaileyHalfZ);
-                Assert.AreEqual(plan.WallHeight, resolved.WallHeight);
-                Assert.AreEqual(plan.WallThickness, resolved.WallThickness);
-                Assert.AreEqual(plan.TowerRadius, resolved.TowerRadius);
-                Assert.AreEqual(plan.GateTowerRadius, resolved.GateTowerRadius);
+                Assert.AreEqual(GameMaterialIds.DarkStone,
+                    config.Palette.Resolve(StructureMaterialRole.Foundation));
+                Assert.AreEqual(GameMaterialIds.Stone,
+                    config.Palette.Resolve(StructureMaterialRole.PrimaryWall));
+                Assert.AreEqual(GameMaterialIds.Slate,
+                    config.Palette.Resolve(StructureMaterialRole.Roof));
+                Assert.AreEqual(GameMaterialIds.Empty,
+                    config.Palette.Resolve(StructureMaterialRole.Opening));
             });
         }
 
@@ -59,19 +70,24 @@ namespace Game.Structures.Tests
         public void CompatibilityProjectionIsPureForSamePlan()
         {
             CastlePlan plan = CastlePlanner.Plan(int3.zero, 99u);
-            CastleConfig a = CastlePresets.Compatibility(in plan);
-            CastleConfig b = CastlePresets.Compatibility(in plan);
+            CastleComponentConfig a = CastleCompatibilityComponents.Resolve(in plan);
+            CastleComponentConfig b = CastleCompatibilityComponents.Resolve(in plan);
 
             Assert.Multiple(() =>
             {
                 Assert.AreEqual(a.KeepFoundation.Primary.Min, b.KeepFoundation.Primary.Min);
                 Assert.AreEqual(a.KeepFoundation.Primary.Size, b.KeepFoundation.Primary.Size);
+                Assert.AreEqual(a.KeepWalls.Height, b.KeepWalls.Height);
+                Assert.AreEqual(a.KeepFloors.FloorCount, b.KeepFloors.FloorCount);
                 Assert.AreEqual(a.CurtainWallX.Length, b.CurtainWallX.Length);
                 Assert.AreEqual(a.CurtainWallX.Height, b.CurtainWallX.Height);
                 Assert.AreEqual(a.CornerTowers.Radius, b.CornerTowers.Radius);
                 Assert.AreEqual(a.CornerTowers.Height, b.CornerTowers.Height);
+                Assert.AreEqual(a.GateTowers.Radius, b.GateTowers.Radius);
                 Assert.AreEqual(a.MainGate.Width, b.MainGate.Width);
-                Assert.AreEqual(a.CurtainBattlements.MerlonWidth, b.CurtainBattlements.MerlonWidth);
+                Assert.AreEqual(a.CurtainBattlements.MerlonWidth,
+                    b.CurtainBattlements.MerlonWidth);
+                Assert.AreEqual(a.Palette.PrimaryWall, b.Palette.PrimaryWall);
             });
         }
     }
