e9736962c2b8e3ca9a0e0906594e6d400e4d3866
jlashmet <jlashmet@gmail.com>
2026-08-17 09:33:46 -1000

    feat(character-factory): expose backend profile catalogue

--- tools/character-factory/api/__init__.py
+++ tools/character-factory/api/__init__.py
@@ -1,3 +1,11 @@
+from .backend_profiles import backend_profile, backend_profiles
 from .models import AssetType, BuildSpec, CharacterFactoryError, GeneratorBackend
 
-__all__ = ["AssetType", "BuildSpec", "CharacterFactoryError", "GeneratorBackend"]
+__all__ = [
+    "AssetType",
+    "BuildSpec",
+    "CharacterFactoryError",
+    "GeneratorBackend",
+    "backend_profile",
+    "backend_profiles",
+]
