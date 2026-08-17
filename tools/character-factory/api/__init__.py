eec0a42af095e90568c791a2cd4e6761c9df6b4a
jlashmet <jlashmet@gmail.com>
2026-08-17 09:54:45 -1000

    feat(character-factory): export appearance strategy API

--- tools/character-factory/api/__init__.py
+++ tools/character-factory/api/__init__.py
@@ -1,11 +1,19 @@
+from .appearance_profiles import (
+    AppearanceStrategy,
+    appearance_strategies,
+    default_appearance_strategy,
+)
 from .backend_profiles import backend_profile, backend_profiles
 from .models import AssetType, BuildSpec, CharacterFactoryError, GeneratorBackend
 
 __all__ = [
+    "AppearanceStrategy",
     "AssetType",
     "BuildSpec",
     "CharacterFactoryError",
     "GeneratorBackend",
+    "appearance_strategies",
     "backend_profile",
     "backend_profiles",
+    "default_appearance_strategy",
 ]
