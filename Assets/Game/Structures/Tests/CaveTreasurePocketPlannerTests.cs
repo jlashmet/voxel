2bcdd3643d2b77300baf6c53a0879de4eaa9a8e4
jlashmet <jlashmet@gmail.com>
2026-08-18 14:00:42 -1000

    Prove treasure pocket against authored cave terminals

--- Assets/Game/Structures/Tests/CaveTreasurePocketPlannerTests.cs
+++ Assets/Game/Structures/Tests/CaveTreasurePocketPlannerTests.cs
@@ -4,6 +4,7 @@ using NUnit.Framework;
 using Unity.Mathematics;
 using VoxelEngine.Storage.Api;
 using VoxelEngine.Structures.Api;
+using VoxelEngine.Structures.Runtime;
 
 namespace Game.Structures.Tests
 {
@@ -43,6 +44,58 @@ namespace Game.Structures.Tests
             });
         }
 
+        [Test]
+        public void AuthoredCaveTerminalLeavesVerifiedRockForTreasurePocket()
+        {
+            var world = new SolidWorldSession();
+            CaveConfig cave = CaveConfig.Default;
+            cave.TunnelWidth = 5;
+            cave.TunnelHeight = 7;
+            cave.SegmentLength = 8;
+            cave.MainSegmentCount = 3;
+            cave.TurnChancePercent = 0;
+            cave.VerticalChancePercent = 0;
+            cave.ChamberChancePercent = 0;
+            cave.BranchChancePercent = 100;
+            cave.MaxBranches = 1;
+            cave.MaxBranchDepth = 1;
+            cave.BranchSegmentCount = 2;
+            cave.MinBranchSeparation = 0;
+            cave.WallRoughness = 0;
+            cave.FloorRoughness = 0;
+            cave.CeilingRoughness = 0;
+
+            CaveGenerationRequest request = CaveGenerationRequest.Attached(
+                0x12345678ul, int3.zero, Facing.North, 5, 7, 2);
+            var palette = new CaveMaterialPalette { Opening = 0, Rock = 2 };
+            CaveAuthoringResult authored = CaveAuthoring.Author(
+                world, in request, in cave, in palette);
+            CaveSecretPocketConfig pocketConfig = PocketConfig();
+
+            Assert.GreaterOrEqual(authored.TraversalCandidates.Count, 2,
+                "Configured authored cave should expose a main and branch terminal.");
+            Assert.IsTrue(CaveTreasurePocketPlanner.TryAuthorBest(
+                world,
+                in authored.TraversalCandidates,
+                8,
+                in pocketConfig,
+                out CaveGameplayPlacement placement,
+                out CaveSecretPocket pocket));
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(placement.Terminal.Flags & CaveTraversalFlags.Branch,
+                    Is.EqualTo(CaveTraversalFlags.Branch));
+                Assert.IsTrue(pocket.IsWellFormed);
+                Assert.IsTrue(AllSolid(world, in pocket.Barrier),
+                    "The actual cave authorer must leave the retained false-wall slab as rock.");
+                Assert.IsTrue(AllEmpty(world, in pocket.Connector));
+                Assert.IsTrue(AllEmpty(world, in pocket.Pocket));
+                Assert.AreEqual(2, world.CarveCalls,
+                    "Cave authoring itself uses cross-section fills; only the secret connector and pocket use Carve.");
+            });
+        }
+
         private static CaveTraversalCandidate Branch(int3 position, int distance, Facing facing) =>
             new CaveTraversalCandidate
             {
@@ -66,6 +119,24 @@ namespace Game.Structures.Tests
             PocketDepth = 9,
         };
 
+        private static bool AllSolid(SolidWorldSession world, in Game.Structures.Api.DecorationBounds bounds)
+        {
+            for (int y = bounds.Min.y; y < bounds.MaxExclusive.y; y++)
+            for (int z = bounds.Min.z; z < bounds.MaxExclusive.z; z++)
+            for (int x = bounds.Min.x; x < bounds.MaxExclusive.x; x++)
+                if (!world.IsSolid(x, y, z)) return false;
+            return true;
+        }
+
+        private static bool AllEmpty(SolidWorldSession world, in Game.Structures.Api.DecorationBounds bounds)
+        {
+            for (int y = bounds.Min.y; y < bounds.MaxExclusive.y; y++)
+            for (int z = bounds.Min.z; z < bounds.MaxExclusive.z; z++)
+            for (int x = bounds.Min.x; x < bounds.MaxExclusive.x; x++)
+                if (world.IsSolid(x, y, z)) return false;
+            return true;
+        }
+
         private sealed class SolidWorldSession : IStructureAuthoringSession
         {
             private readonly HashSet<int3> _empty = new HashSet<int3>();
@@ -88,9 +159,19 @@ namespace Game.Structures.Tests
                 byte coating = Coatings.None, VoxelSurfaceFlags flags = VoxelSurfaceFlags.None) =>
                 Set(x, y, z, material);
             public void Coat(int x, int y, int z, byte coating) { }
-            public void FillBulk(int3 min, int3 size, byte material) { }
-            public void FillColumnBulk(int x, int minY, int maxYExclusive, int z, byte material) { }
-            public void Box(int3 min, int3 size, byte material) { }
+            public void FillBulk(int3 min, int3 size, byte material)
+            {
+                for (int y = min.y; y < min.y + size.y; y++)
+                for (int z = min.z; z < min.z + size.z; z++)
+                for (int x = min.x; x < min.x + size.x; x++)
+                    Set(x, y, z, material);
+            }
+            public void FillColumnBulk(int x, int minY, int maxYExclusive, int z, byte material)
+            {
+                for (int y = minY; y < maxYExclusive; y++)
+                    Set(x, y, z, material);
+            }
+            public void Box(int3 min, int3 size, byte material) => FillBulk(min, size, material);
             public void HollowBox(int3 min, int3 size, int thickness, byte material, bool floor, bool ceiling) { }
             public void Cylinder(int cx, int baseY, int cz, int radius, int height, byte material, int innerRadius = 0) { }
             public void Disc(int cx, int y, int cz, int radius, byte material) { }
@@ -105,10 +186,7 @@ namespace Game.Structures.Tests
             public void Carve(int3 min, int3 size)
             {
                 CarveCalls++;
-                for (int y = min.y; y < min.y + size.y; y++)
-                for (int z = min.z; z < min.z + size.z; z++)
-                for (int x = min.x; x < min.x + size.x; x++)
-                    _empty.Add(new int3(x, y, z));
+                FillBulk(min, size, 0);
             }
             public void Weather(int3 min, int3 size, byte coating, uint seed, int chanceOutOf100) { }
         }
