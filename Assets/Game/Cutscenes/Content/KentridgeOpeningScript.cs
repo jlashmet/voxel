b81c003ab7ca5a0f0ca916692c385066fd8701fc
jlashmet <jlashmet@gmail.com>
2026-08-21 16:33:54 -1000

    Separate recovered Kentridge speakers from dialogue text

--- Assets/Game/Cutscenes/Content/KentridgeOpeningScript.cs
+++ Assets/Game/Cutscenes/Content/KentridgeOpeningScript.cs
@@ -7,9 +7,10 @@ namespace Game.Cutscenes.Content.Kentridge
     /// <summary>
     /// Spoken content for the Kentridge opening.
     ///
-    /// Lines 1-31 are transcribed verbatim from the original MountingForce Art/Opening.txt.
-    /// The choreography consumes stable cue ids while presentation resolves those ids here, so
-    /// dialogue content remains separate from staging and timing.
+    /// Lines 1-31 preserve the spoken text from the original MountingForce Art/Opening.txt.
+    /// Speaker labels from that source file are represented by the cutscene dialogue steps rather
+    /// than repeated in the displayed text. The choreography consumes stable cue ids while
+    /// presentation resolves those ids here, keeping dialogue content separate from staging.
     /// </summary>
     public static class KentridgeOpeningScript
     {
@@ -19,37 +20,37 @@ namespace Game.Cutscenes.Content.Kentridge
 
         private static readonly string[] OriginalOpeningLines =
         {
-            "Madeline: Hey Weldon, Glad you could join us!  How did your magic lessons go?  It looks like Medrare kept you late again.",
-            "Weldon: Do you have to rub it in?  You know I was forced into wizardry, and I hate every second of it!",
-            "Weldon: Just because my father was a wizard, now I have to get stuck with it too?  What kind of crazy system is that?",
-            "Madeline: But you're so talented! The rest of the students would give anything to have your skills.",
-            "Weldon: Anyway, man I'm starving.  I wish I could get a decent meal in this town.",
-            "Steven: Bah, you probably could if it weren't for Lord Radcliffe.",
-            "Steven: I see servants bringing food to his house day in and day out.",
-            "Madeline: Hey Weldon, why don't you go cast a spell on him?",
-            "Weldon: Madeline geez, can we drop the magic talk already?",
-            "Madeline: Sorry, tee hee.  Here, next round is on me.",
-            "Logan: Pardon me friends, I don't believe weve met before.",
-            "Logan: My name is Logan. I couldn't help overhearing your conversation, and I'd like to tell you there are many more in this town that share your sentiments.",
-            "Madeline: Yah its pretty hard not to feel that way when you're starving and forced to watch fatty Radcliffe gorge himself.",
-            "Logan: I've been trying to gather support to try and take Radcliffe down.  However, its been difficult.",
-            "Logan: Even though most of the town is of the same mind, I've yet to find anyone that will help me take action.",
-            "Weldon: So... what are you planning?",
-            "Logan: Well, I'm a reasonable man.  I'd like to schedule a meeting with Lord Radcliffe and inquire about the official supply distribution policies.",
-            "Logan: I'm hoping he owns up to his squandering, and that we can reach an agreement peacefully.",
-            "Logan: However, I'd like you all to accompany me as lets say... an insurance policy.",
-            "Steven: So you want us to march into an elected officials house like some brute squad and take care of your dirty work?  Unthinkable!",
-            "Logan: Look, I can tell by your appearance that you are an ordained knight.  And apparently, one so dedicated that you feel the need to wear your helmet in doors.",
-            "Logan: If Lord Radcliffe is breaking the law by stealing food and supplies, isn't it your duty to uphold the law?",
-            "Steven: Well, I suppose so... What do you guys think?",
-            "Weldon: I'm in.  Its about time Radcliffe was confronted.",
-            "Madeline: Me too.  Its time the people got their fair share and I'm sure he will listen to reason.",
-            "Weldon: Ok then.  It's settled.  Logan, we're with you.",
-            "Weldon: There's a few things I have to do first though.  First, my father wanted me to stop by the house to show me something.",
-            "Weldon: Do you mind doing that first?",
-            "Logan: For my new companions? Anything!",
-            "Weldon: Ok, my house is in the southwest corner of the town just north of the church.",
-            "Logan: Alright friends.  Lets head out!"
+            "Hey Weldon, Glad you could join us!  How did your magic lessons go?  It looks like Medrare kept you late again.",
+            "Do you have to rub it in?  You know I was forced into wizardry, and I hate every second of it!",
+            "Just because my father was a wizard, now I have to get stuck with it too?  What kind of crazy system is that?",
+            "But you're so talented! The rest of the students would give anything to have your skills.",
+            "Anyway, man I'm starving.  I wish I could get a decent meal in this town.",
+            "Bah, you probably could if it weren't for Lord Radcliffe.",
+            "I see servants bringing food to his house day in and day out.",
+            "Hey Weldon, why don't you go cast a spell on him?",
+            "Madeline geez, can we drop the magic talk already?",
+            "Sorry, tee hee.  Here, next round is on me.",
+            "Pardon me friends, I don't believe weve met before.",
+            "My name is Logan. I couldn't help overhearing your conversation, and I'd like to tell you there are many more in this town that share your sentiments.",
+            "Yah its pretty hard not to feel that way when you're starving and forced to watch fatty Radcliffe gorge himself.",
+            "I've been trying to gather support to try and take Radcliffe down.  However, its been difficult.",
+            "Even though most of the town is of the same mind, I've yet to find anyone that will help me take action.",
+            "So... what are you planning?",
+            "Well, I'm a reasonable man.  I'd like to schedule a meeting with Lord Radcliffe and inquire about the official supply distribution policies.",
+            "I'm hoping he owns up to his squandering, and that we can reach an agreement peacefully.",
+            "However, I'd like you all to accompany me as lets say... an insurance policy.",
+            "So you want us to march into an elected officials house like some brute squad and take care of your dirty work?  Unthinkable!",
+            "Look, I can tell by your appearance that you are an ordained knight.  And apparently, one so dedicated that you feel the need to wear your helmet in doors.",
+            "If Lord Radcliffe is breaking the law by stealing food and supplies, isn't it your duty to uphold the law?",
+            "Well, I suppose so... What do you guys think?",
+            "I'm in.  Its about time Radcliffe was confronted.",
+            "Me too.  Its time the people got their fair share and I'm sure he will listen to reason.",
+            "Ok then.  It's settled.  Logan, we're with you.",
+            "There's a few things I have to do first though.  First, my father wanted me to stop by the house to show me something.",
+            "Do you mind doing that first?",
+            "For my new companions? Anything!",
+            "Ok, my house is in the southwest corner of the town just north of the church.",
+            "Alright friends.  Lets head out!"
         };
 
         private static readonly Dictionary<string, string> AdditionalLines = new Dictionary<string, string>
@@ -69,7 +70,7 @@ namespace Game.Cutscenes.Content.Kentridge
         }
 
         /// <summary>
-        /// The line for a cue, or a visible placeholder naming the cue.
+        /// The spoken text for a cue, or a visible placeholder naming the cue.
         /// </summary>
         public static string LineFor(CutsceneCueId cue)
         {
