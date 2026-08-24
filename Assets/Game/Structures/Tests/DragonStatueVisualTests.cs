88c25f72fae8088df10872667b205feedd7ff0e4
jlashmet <jlashmet@gmail.com>
2026-08-24 13:24:57 -0700

    Render dragon through world builder voxel stamp path

--- Assets/Game/Structures/Tests/DragonStatueVisualTests.cs
+++ Assets/Game/Structures/Tests/DragonStatueVisualTests.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using Game.Structures.Api;
 using Game.Structures.Runtime;
 using NUnit.Framework;
 using Unity.Mathematics;
@@ -13,8 +14,28 @@ namespace Game.Structures.Tests
             int3 min = DragonStatueAuthoring.LocalMin + new int3(-4, -4, -4);
             int3 size = DragonStatueAuthoring.LocalSize + new int3(8, 8, 8);
             var capture = new VisualStructureCapture(min, size);
+            var placement = DragonStatueWorldBuilderObject.CreatePlacement(
+                new GeneratedPropId(0xD12A60UL),
+                sceneId: 1,
+                slotId: 1,
+                origin: int3.zero,
+                facing: new int3(0, 0, -1));
+            var context = new DecorationContext
+            {
+                WorldSeed = 0xD12A60u,
+                StructureId = 1,
+                SpaceId = 1,
+                StyleId = 1,
+                StructureKind = DecorationStructureKind.Castle,
+                SpaceKind = DecorationSpaceKind.ExteriorYard,
+                Wealth = DecorationWealthTier.Noble,
+                Condition = DecorationConditionTier.Worn,
+                Environment = DecorationEnvironmentTags.Exterior,
+            };
 
-            DragonStatueAuthoring.Author(capture, int3.zero);
+            Assert.That(DragonStatueWorldBuilderObject.Descriptor.IsWellFormed, Is.True);
+            Assert.That(placement.IsWellFormed, Is.True);
+            Assert.That(DecorationVoxelStampBackend.TryAuthor(capture, in placement, in context), Is.True);
 
             Assert.That(capture.TotalVoxelsWritten, Is.GreaterThan(20000),
                 "Dragon statue should contain enough sampled implicit detail to read as a sculpt.");
