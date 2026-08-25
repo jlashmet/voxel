3b140d8abe0faee8fb6510b0d250e47594c72ddd
jlashmet <jlashmet@gmail.com>
2026-08-25 09:59:51 -0700

    SceneIssue 040805: grant WorldBuilder backend friend access

--- Assets/Game/WorldBuilder/Api/AssemblyInfo.cs
+++ Assets/Game/WorldBuilder/Api/AssemblyInfo.cs
@@ -1,3 +1,6 @@
 using System.Runtime.CompilerServices;
 
+[assembly: InternalsVisibleTo("Game.WorldBuilder.Runtime")]
+[assembly: InternalsVisibleTo("Game.WorldBuilder.Voxel")]
+[assembly: InternalsVisibleTo("Game.Composition.WorldBuilderWorldGen.Runtime")]
 [assembly: InternalsVisibleTo("VoxelEngine.Tests.EditMode")]
