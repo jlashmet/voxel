2bdda6b1380091660c11cabfb26ceb231570de45
jlashmet <jlashmet@gmail.com>
2026-08-25 02:46:11 -0700

    fix(scene): remove disconnected lower Kentridge stair

--- Packages/com.mountingforce.worldgen/Runtime/Content/Kentridge/KentridgeUrbanCirculation.cs
+++ Packages/com.mountingforce.worldgen/Runtime/Content/Kentridge/KentridgeUrbanCirculation.cs
@@ -44,11 +44,6 @@ namespace MountingForce.WorldGen.Content.Kentridge
         public const int UpperContourZDm = 340;
         public const int UpperContourWidthDm = 40;
 
-        public const int LowerWestStairXDm = 1125;
-        public const int LowerWestStairSouthZDm = 900;
-        public const int LowerWestStairNorthZDm = 590;
-        public const int LowerWestStairWidthDm = 22;
-
         // Tuck the Market-to-Upper stair into the urban fabric: west of the upper-west block's x=850
         // edge but far enough east to read as a stair street between buildings rather than a detached
         // hillside stair at the capture/world edge.
@@ -65,10 +60,9 @@ namespace MountingForce.WorldGen.Content.Kentridge
             int mainWestEdge = KentridgeTownPlanner.MainSpineXDm - KentridgeTownPlanner.MainRoadWidthDm / 2;
             int eastLaneWestEdge = KentridgeTownPlanner.EastLaneXDm - KentridgeTownPlanner.ServiceRoadWidthDm / 2;
 
-            var connectors = new List<KentridgeUrbanConnector>(4)
+            var connectors = new List<KentridgeUrbanConnector>(3)
             {
                 new KentridgeUrbanConnector("upper-east-contour", KentridgeUrbanConnectorKind.ContourLane, KentridgeUrbanBand.UpperWard, new Int2(mainEastEdge, UpperContourZDm), new Int2(eastLaneWestEdge, UpperContourZDm), UpperContourWidthDm),
-                new KentridgeUrbanConnector("lower-west-stair-street", KentridgeUrbanConnectorKind.StairStreet, KentridgeUrbanBand.LowerWard, new Int2(LowerWestStairXDm, LowerWestStairSouthZDm), new Int2(LowerWestStairXDm, LowerWestStairNorthZDm), LowerWestStairWidthDm),
                 new KentridgeUrbanConnector("west-upper-stair-street", KentridgeUrbanConnectorKind.StairStreet, KentridgeUrbanBand.UpperWard, new Int2(WestUpperStairXDm, WestUpperStairSouthZDm), new Int2(WestUpperStairXDm, WestUpperStairNorthZDm), WestUpperStairWidthDm),
                 new KentridgeUrbanConnector("west-upper-contour", KentridgeUrbanConnectorKind.ContourLane, KentridgeUrbanBand.UpperWard, new Int2(WestUpperStairXDm, UpperContourZDm), new Int2(mainWestEdge, UpperContourZDm), WestUpperContourWidthDm),
             };
