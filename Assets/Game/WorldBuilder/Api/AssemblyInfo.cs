ae6e386101c4073a8921cdf74cb981265dc9a04d
jlashmet <jlashmet@gmail.com>
2026-08-28 11:41:45 -0700

    Keep WorldBuilder test friend boundary minimal

--- Assets/Game/WorldBuilder/Api/AssemblyInfo.cs
+++ Assets/Game/WorldBuilder/Api/AssemblyInfo.cs
@@ -5,4 +5,3 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("Game.Composition.WorldBuilderWorldGen.Runtime")]
 [assembly: InternalsVisibleTo("Game.Kentridge.PlayableSlice")]
 [assembly: InternalsVisibleTo("VoxelEngine.Tests.EditMode")]
-[assembly: InternalsVisibleTo("VoxelEngine.Tests.PlayMode")]
