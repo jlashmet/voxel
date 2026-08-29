e7d308ef421c579a5dbed98af3eb52bc7ad84104
jlashmet <jlashmet@gmail.com>
2026-08-29 02:18:02 -0700

    feat: wire source-backed Medrare opening sequence

--- Assets/Game/Composition/Campaign/Content/KnownOpeningCampaignContent.cs
+++ Assets/Game/Composition/Campaign/Content/KnownOpeningCampaignContent.cs
@@ -13,6 +13,7 @@ namespace Game.Composition.Campaign.Content
         public SiteHandle FirstDestination { get; }
         public SiteHandle AwonSite { get; }
         public SiteHandle MedrareSite { get; }
+        public SiteHandle MedrareHouseSite { get; }
         public NpcHandle Madeline { get; }
         public NpcHandle Steven { get; }
         public NpcHandle Logan { get; }
@@ -21,14 +22,23 @@ namespace Game.Composition.Campaign.Content
         public NpcHandle Medrare { get; }
 
         internal KnownOpeningCampaignRoles(
-            SiteHandle startingPub, SiteHandle firstDestination, SiteHandle awonSite, SiteHandle medrareSite,
-            NpcHandle madeline, NpcHandle steven, NpcHandle logan, NpcHandle destinationNpc,
-            NpcHandle awon, NpcHandle medrare)
+            SiteHandle startingPub,
+            SiteHandle firstDestination,
+            SiteHandle awonSite,
+            SiteHandle medrareSite,
+            SiteHandle medrareHouseSite,
+            NpcHandle madeline,
+            NpcHandle steven,
+            NpcHandle logan,
+            NpcHandle destinationNpc,
+            NpcHandle awon,
+            NpcHandle medrare)
         {
             StartingPub = startingPub ?? throw new ArgumentNullException(nameof(startingPub));
             FirstDestination = firstDestination ?? throw new ArgumentNullException(nameof(firstDestination));
             AwonSite = awonSite ?? throw new ArgumentNullException(nameof(awonSite));
             MedrareSite = medrareSite ?? throw new ArgumentNullException(nameof(medrareSite));
+            MedrareHouseSite = medrareHouseSite ?? throw new ArgumentNullException(nameof(medrareHouseSite));
             Madeline = madeline ?? throw new ArgumentNullException(nameof(madeline));
             Steven = steven ?? throw new ArgumentNullException(nameof(steven));
             Logan = logan ?? throw new ArgumentNullException(nameof(logan));
@@ -46,6 +56,7 @@ namespace Game.Composition.Campaign.Content
         public SiteRef FirstDestination => Roles.FirstDestination.Ref;
         public SiteRef AwonSite => Roles.AwonSite.Ref;
         public SiteRef MedrareSite => Roles.MedrareSite.Ref;
+        public SiteRef MedrareHouseSite => Roles.MedrareHouseSite.Ref;
         public NpcRef Madeline => Roles.Madeline.Ref;
         public NpcRef Steven => Roles.Steven.Ref;
         public NpcRef Logan => Roles.Logan.Ref;
@@ -56,6 +67,7 @@ namespace Game.Composition.Campaign.Content
         public CutsceneRef IntroCutscene { get; }
         public CutsceneRef AwonOpeningCutscene { get; }
         public CutsceneRef SeeMedrareCutscene { get; }
+        public CutsceneRef MedrareJoinCutscene { get; }
         public CutsceneRef MedrareFirstSpellCutscene { get; }
         public CutsceneRef MedrareToChurchCutscene { get; }
         public CutsceneRef DestinationCutscene { get; }
@@ -68,6 +80,7 @@ namespace Game.Composition.Campaign.Content
             CutsceneRef introCutscene,
             CutsceneRef awonOpeningCutscene,
             CutsceneRef seeMedrareCutscene,
+            CutsceneRef medrareJoinCutscene,
             CutsceneRef medrareFirstSpellCutscene,
             CutsceneRef medrareToChurchCutscene,
             CutsceneRef destinationCutscene)
@@ -78,6 +91,7 @@ namespace Game.Composition.Campaign.Content
             IntroCutscene = introCutscene;
             AwonOpeningCutscene = awonOpeningCutscene;
             SeeMedrareCutscene = seeMedrareCutscene;
+            MedrareJoinCutscene = medrareJoinCutscene;
             MedrareFirstSpellCutscene = medrareFirstSpellCutscene;
             MedrareToChurchCutscene = medrareToChurchCutscene;
             DestinationCutscene = destinationCutscene;
@@ -98,7 +112,9 @@ namespace Game.Composition.Campaign.Content
             var game = Game.WorldBuilder.Api.Campaign.Create("main-campaign");
             RecoveredOverworldRegionDefinition recoveredKentridge = RecoveredMountingForceWorldCatalog.Kentridge;
             RegionHandle kentridgeOverworld = game.World.Region(recoveredKentridge.RegionId);
-            SettlementHandle kentridge = kentridgeOverworld.Settlement(recoveredKentridge.SettlementId, recoveredKentridge.SettlementArchetype);
+            SettlementHandle kentridge = kentridgeOverworld.Settlement(
+                recoveredKentridge.SettlementId,
+                recoveredKentridge.SettlementArchetype);
 
             SiteHandle startingPub = kentridge.Pub(
                     "starting-pub",
@@ -111,8 +127,14 @@ namespace Game.Composition.Campaign.Content
                 "awon-the-immortal",
                 site => site.DifferentSiteFrom(startingPub).ReachableFrom(startingPub, TraversalProfile.NormalParty));
             SiteHandle medrareSite = kentridgeOverworld.Site(
-                "medrare",
+                "kentridge-medrare-encounter",
                 site => site.DifferentSiteFrom(startingPub).ReachableFrom(startingPub, TraversalProfile.NormalParty));
+            SiteHandle medrareHouseSite = kentridgeOverworld.Site(
+                "medrare-house-lower",
+                site => site
+                    .DifferentSiteFrom(medrareSite)
+                    .ReachableFrom(startingPub, TraversalProfile.NormalParty)
+                    .ReachableFrom(medrareSite, TraversalProfile.NormalParty));
 
             NpcHandle madeline = startingPub.Npc("madeline");
             NpcHandle steven = startingPub.Npc("steven");
@@ -122,8 +144,17 @@ namespace Game.Composition.Campaign.Content
             NpcHandle medrare = medrareSite.Npc("medrare", npc => npc.RequireConversation());
 
             var roles = new KnownOpeningCampaignRoles(
-                startingPub, firstDestination, awonSite, medrareSite,
-                madeline, steven, logan, destinationNpc, awon, medrare);
+                startingPub,
+                firstDestination,
+                awonSite,
+                medrareSite,
+                medrareHouseSite,
+                madeline,
+                steven,
+                logan,
+                destinationNpc,
+                awon,
+                medrare);
 
             ObjectiveHandle travelObjective = firstDestination.Objective(
                 "travel-to-first-destination",
@@ -141,16 +172,21 @@ namespace Game.Composition.Campaign.Content
                     .Bind(KentridgeOpeningCutscene.Logan, logan));
             CutsceneHandle awonOpening = awonSite.Cutscene(
                 KentridgeOpeningProgressionCutscenes.AwonDefinition,
-                scene => scene.Bind(KentridgeOpeningProgressionCutscenes.Awon, awon));
+                scene => scene
+                    .Bind(KentridgeOpeningProgressionCutscenes.Weldon, PlayerSlot.First)
+                    .Bind(KentridgeOpeningProgressionCutscenes.Awon, awon)
+                    .Bind(KentridgeOpeningProgressionCutscenes.Steven, steven)
+                    .Bind(KentridgeOpeningProgressionCutscenes.Madeline, madeline)
+                    .Bind(KentridgeOpeningProgressionCutscenes.Logan, logan));
             CutsceneHandle seeMedrare = medrareSite.Cutscene(
-                KentridgeOpeningProgressionCutscenes.SeeMedrareDefinition,
+                KentridgeOpeningProgressionCutscenes.SeeMedrareDefinition);
+            CutsceneHandle medrareJoin = medrareSite.Cutscene(
+                KentridgeOpeningProgressionCutscenes.MedrareJoinDefinition,
                 scene => scene.Bind(KentridgeOpeningProgressionCutscenes.Medrare, medrare));
-            CutsceneHandle medrareFirstSpell = medrareSite.Cutscene(
-                KentridgeOpeningProgressionCutscenes.MedrareFirstSpellDefinition,
-                scene => scene.Bind(KentridgeOpeningProgressionCutscenes.Medrare, medrare));
-            CutsceneHandle medrareToChurch = medrareSite.Cutscene(
-                KentridgeOpeningProgressionCutscenes.MedrareToChurchDefinition,
-                scene => scene.Bind(KentridgeOpeningProgressionCutscenes.Logan, logan));
+            CutsceneHandle medrareFirstSpell = medrareHouseSite.Cutscene(
+                KentridgeOpeningProgressionCutscenes.MedrareFirstSpellDefinition);
+            CutsceneHandle medrareToChurch = medrareHouseSite.Cutscene(
+                KentridgeOpeningProgressionCutscenes.MedrareToChurchDefinition);
 
             game.Story.Rule("start-intro", rule => rule
                 .When(StoryTrigger.NewGame())
@@ -167,15 +203,26 @@ namespace Game.Composition.Campaign.Content
                 .If(StoryCondition.CutsceneNotCompleted(awonOpening))
                 .Then(StoryEffect.PlayCutscene(awonOpening)));
             game.Story.Rule("see-medrare-after-awon", rule => rule
-                .When(StoryTrigger.InteractWith(medrare))
+                .When(StoryTrigger.EnterSiteProximity(medrareSite))
                 .If(StoryCondition.CutsceneCompleted(awonOpening))
                 .If(StoryCondition.CutsceneNotCompleted(seeMedrare))
                 .Then(StoryEffect.PlayCutscene(seeMedrare)));
-            game.Story.Rule("medrare-first-spell-on-return", rule => rule
+            game.Story.Rule("medrare-join-after-awon", rule => rule
                 .When(StoryTrigger.InteractWith(medrare))
-                .If(StoryCondition.CutsceneCompleted(seeMedrare))
+                .If(StoryCondition.CutsceneCompleted(awonOpening))
+                .If(StoryCondition.CutsceneNotCompleted(medrareJoin))
+                .Then(StoryEffect.PlayCutscene(medrareJoin)));
+            game.Story.Rule("persist-medrare-join", rule => rule
+                .When(StoryTrigger.CutsceneCompleted(medrareJoin))
+                .Then(StoryEffect.JoinPartyMember("Medrare")));
+            game.Story.Rule("medrare-first-spell-after-awon", rule => rule
+                .When(StoryTrigger.EnterSiteProximity(medrareHouseSite))
+                .If(StoryCondition.CutsceneCompleted(awonOpening))
                 .If(StoryCondition.CutsceneNotCompleted(medrareFirstSpell))
                 .Then(StoryEffect.PlayCutscene(medrareFirstSpell)));
+            game.Story.Rule("grant-flame-after-first-spell", rule => rule
+                .When(StoryTrigger.CutsceneCompleted(medrareFirstSpell))
+                .Then(StoryEffect.GrantSpell("Flame")));
             game.Story.Rule("medrare-to-church-after-first-spell", rule => rule
                 .When(StoryTrigger.CutsceneCompleted(medrareFirstSpell))
                 .If(StoryCondition.CutsceneNotCompleted(medrareToChurch))
@@ -187,8 +234,16 @@ namespace Game.Composition.Campaign.Content
                 .Then(StoryEffect.PlayCutscene(destinationCutscene)));
 
             return new KnownOpeningCampaignContent(
-                game.Build(), roles, travelObjective, introCutscene,
-                awonOpening, seeMedrare, medrareFirstSpell, medrareToChurch, destinationCutscene);
+                game.Build(),
+                roles,
+                travelObjective,
+                introCutscene,
+                awonOpening,
+                seeMedrare,
+                medrareJoin,
+                medrareFirstSpell,
+                medrareToChurch,
+                destinationCutscene);
         }
     }
 }
