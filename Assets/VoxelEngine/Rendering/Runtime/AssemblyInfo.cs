95de8842221b777050294649168b2224fa5291d8
jlashmet <jlashmet@gmail.com>
2026-08-31 22:01:07 -0700

    Fix Rendering test friend assemblies after module migration

--- Assets/VoxelEngine/Rendering/Runtime/AssemblyInfo.cs
+++ Assets/VoxelEngine/Rendering/Runtime/AssemblyInfo.cs
@@ -3,3 +3,6 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("VoxelEngine.Tests.EditMode")]
 [assembly: InternalsVisibleTo("VoxelEngine.Tests.PlayMode")]
 [assembly: InternalsVisibleTo("VoxelEngine.CI.PlayMode")]
+[assembly: InternalsVisibleTo("VoxelEngine.Rendering.Tests.EditMode")]
+[assembly: InternalsVisibleTo("VoxelEngine.Storage.Tests.EditMode")]
+[assembly: InternalsVisibleTo("Game.WorldBuilder.Tests.EditMode")]
