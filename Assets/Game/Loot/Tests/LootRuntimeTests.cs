9c474a5764c672ece2b2c2519763aa3613992d72
jlashmet <jlashmet@gmail.com>
2026-09-02 22:46:10 -0700

    Cover competing GameSystem10 container transfers

--- Assets/Game/Loot/Tests/LootRuntimeTests.cs
+++ Assets/Game/Loot/Tests/LootRuntimeTests.cs
@@ -125,6 +125,39 @@ namespace Game.Loot.Tests
         }
 
         [Test]
+        public void CompetingContainerTransfers_CommitAtMostAvailableQuantityAndConserveTotal()
+        {
+            Assert.That(_inventory.TryAdd(_actorInventory, _item, 1).Succeeded, Is.True);
+            var containerObject = new WorldObjectId("fixture.container.race");
+            var actorA = new CharacterId("actor:a");
+            var actorB = new CharacterId("actor:b");
+            var start = new ManualResetEventSlim(false);
+            LootTransferResult resultA = default;
+            LootTransferResult resultB = default;
+
+            var taskA = Task.Run(() =>
+            {
+                start.Wait();
+                resultA = _loot.TryContainerTransfer(new ContainerTransferRequest(
+                    actorA, containerObject, _actorInventory, _containerInventory, _item, 1));
+            });
+            var taskB = Task.Run(() =>
+            {
+                start.Wait();
+                resultB = _loot.TryContainerTransfer(new ContainerTransferRequest(
+                    actorB, containerObject, _actorInventory, _containerInventory, _item, 1));
+            });
+
+            start.Set();
+            Task.WaitAll(taskA, taskB);
+
+            Assert.That((resultA.Succeeded ? 1 : 0) + (resultB.Succeeded ? 1 : 0), Is.EqualTo(1));
+            Assert.That(_inventory.Count(_actorInventory, _item), Is.Zero);
+            Assert.That(_inventory.Count(_containerInventory, _item), Is.EqualTo(1));
+            Assert.That(_inventory.Count(_actorInventory, _item) + _inventory.Count(_containerInventory, _item), Is.EqualTo(1));
+        }
+
+        [Test]
         public void DropThenPickup_RoundTripsPayloadAndQuantity()
         {
             Assert.That(_inventory.TryAdd(_actorInventory, _item, 4).Succeeded, Is.True);
