657eba3c4e3daf0b3d7a163a78152361977c52d4
jlashmet <jlashmet@gmail.com>
2026-09-03 19:57:37 -0700

    Fix Cave validation storage import

--- Assets/Game/Composition/CaveWorldBuilder/Validation/CaveWorldBuilderSecretPocketValidation.cs
+++ Assets/Game/Composition/CaveWorldBuilder/Validation/CaveWorldBuilderSecretPocketValidation.cs
@@ -7,6 +7,7 @@ using Unity.Mathematics;
 using UnityEngine;
 using VoxelEngine.Composition;
 using VoxelEngine.Showcase;
+using VoxelEngine.Storage.Api;
 using VoxelEngine.Structures.Api;
 using VoxelEngine.Structures.Runtime;
 using VoxelEngine.Terrain.Api;
